2aa5b12894a18ffe4d27a36fb633b0d1ce5715d3
CCWILLDRV247 <chris.williams@drv247.com>
2026-09-20 19:39:38 +0100

    Track engine branch in version.ts

---
+++ lib/engine/version.ts
@@ -0,0 +1,6 @@
+export const ENGINE_BRANCH = "cursor/for-you-personalisation-0c1e";
+export const ENGINE_WAVE = "wave-6-74";
+
+export function engineCommit() {
+  return process.env.ENGINE_COMMIT || process.env.VERCEL_GIT_COMMIT_SHA || "local";
+}
